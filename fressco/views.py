22ca36b05b53e8ab29dfe37602b38181492bcc67
aelsir <ahmed.elsir.khalfalla@gmail.com>
2022-07-05 10:01:39 +0200

    add dashboard and login

--- fressco/views.py
+++ fressco/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'page1/index.html')
+    return render(request,'fressco/index.html')
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'fressco/dashboard.html')
+
+def login(request):
+    return render(request, 'fressco/login.html')
